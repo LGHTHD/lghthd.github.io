696bd141b5a73b66d8b337ca730a3d36fa26fe77
William Bjork <williambjrk@gmail.com>
2018-04-07 20:07:55 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+
+    //Pin scene
+
+
+
+    // build a scene
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '.p2',
+    triggerHook: 0.7
+
+
+  })
+
+  .setClassToggle('.p2', 'fade-in') // add class to title-1
+  .addIndicators()
+  .addTo(controller);
+
+  // build a scene
+var ourScene2 = new ScrollMagic.Scene({
+  triggerElement: '.boi',
+  triggerHook: 0.35
+
+
+})
+
+.setClassToggle('.boi', 'fade-in') // add class to title-1
+.addIndicators()
+.addTo(controller);
+
+var ourScene3 = new ScrollMagic.Scene({
+  triggerElement: '.outline',
+  triggerHook: 0.9
+
+
+})
+
+.setClassToggle('.outline', 'fade-in') // add class to title-1
+.addIndicators()
+.addTo(controller);
+
+// build a scene
+var ourScene3 = new ScrollMagic.Scene({
+triggerElement: '.manman',
+triggerHook: 0.7
+
+
+})
+
+.setClassToggle('.manman', 'fade-in') // add class to title-1
+.addIndicators()
+.addTo(controller);
+
+// build a scene
+var ourScene4 = new ScrollMagic.Scene({
+triggerElement: '.skills',
+triggerHook: 0.7
+
+
+})
+
+.setClassToggle('.skills', 'fade-in') // add class to title-1
+.addIndicators()
+.addTo(controller);
+});
